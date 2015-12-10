c977818f9905aa5452b525dfbe3da6de5d8a34a4
LYXWuDao <2858535420@qq.com>
2015-12-10 20:44:57 +0800

    add  game start files

---
+++ Assets/Scripts/GameApp.cs
@@ -0,0 +1,42 @@
+﻿using LGame.LBehaviour;
+using LGame.LCommon;
+using UnityEngine;
+using System.Collections;
+
+/***
+ * 
+ * 游戏入口
+ * 
+ */
+
+public class GameApp : LABehaviour
+{
+
+    /// <summary>
+    /// 初始化游戏
+    /// </summary>
+    public override void Awake()
+    {
+        // 创建2d ui 相机
+        UIPanel panel = NGUITools.CreateUI(false);
+        UIRoot uiRoot = panel.GetComponent<UIRoot>();
+        uiRoot.scalingStyle = UIRoot.Scaling.Constrained;
+        // 找到创建的 ui 相机
+        UICamera uiCamera = uiRoot.GetComponentInChildren<UICamera>();
+        // 创建所有界面的根节点
+        GameObject root = LCSCompHelper.Create("_ui root", uiCamera.transform);
+    }
+
+    // Use this for initialization
+    public override void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public override void OnUpdate(float deltaTime)
+    {
+
+    }
+
+}
