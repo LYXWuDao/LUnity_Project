d940c310bacc4351f38029592f8500784246335b
LYXWuDao <2858535420@qq.com>
2015-12-19 15:34:20 +0800

    修改登录

--- Assets/Scripts/UI/UiLogin.cs
+++ Assets/Scripts/UI/UiLogin.cs
@@ -23,16 +23,44 @@ public sealed class UiLogin : ALUIBehaviour
     [NonSerialized]
     public GameObject mEnterBtn = null;
 
+    /// <summary>
+    /// 标题
+    /// </summary>
+    [NonSerialized]
+    public GameObject mTitle = null;
+
+    /// <summary>
+    /// 区
+    /// </summary>
+    [NonSerialized]
+    public GameObject mArean = null;
+
     public override void OnAwake()
     {
         mEnterBtn = SLCompHelper.FindGameObject(gameObject, "content/enter_btn");
+        mTitle = SLCompHelper.FindGameObject(gameObject, "bg/title");
+        mArean = SLCompHelper.FindGameObject(gameObject, "content/arean");
+
+        mEnterBtn.SetActive(false);
+        mTitle.SetActive(false);
+        mArean.SetActive(false);
     }
 
     public override void OnStart()
     {
         CLDelayAction.BeginAction(1f, delegate()
         {
-            CLTweenEvent.BeginScale(mEnterBtn, 1f, new Vector3(0.5f, 0.5f, 0.5f), Vector3.one);
+            CLTweenEvent.BeginScale(mTitle, 0.7f, new Vector3(0.5f, 0.5f, 0.5f), Vector3.one * 1.1f);
+            CLTweenEvent.BeginScaleImmediate(mTitle, 0.3f, Vector3.one * 1.1f, Vector3.one);
+
+            CLTweenEvent.BeginScaleImmediate(mArean, 0.35f, Vector3.one * 0.5f, Vector3.one);
+            CLTweenEvent.BeginScale(mEnterBtn, 0.5f, Vector3.one * 0.5f, Vector3.one);
+
+            CLDelayAction.BeginAction(1.3f, delegate()
+            {
+                SLCompHelper.FindComponet<UIButtonScale>(mArean);
+                SLCompHelper.FindComponet<UIButtonScale>(mEnterBtn);
+            });
         });
     }
 
@@ -47,11 +75,15 @@ public sealed class UiLogin : ALUIBehaviour
     /// <summary>
     /// 鼠标单击
     /// </summary>
-    /// <param name="btn"></param>
+    /// <param name="btn">点击的按钮</param>
     public override void OnCollider(GameObject btn)
     {
         if (btn == null) return;
-        SLDebugHelper.WriteError(btn.name);
+        string btnName = btn.name;
+        if (btnName == "enter_btn")
+        {
+
+        }
     }
 
 }
