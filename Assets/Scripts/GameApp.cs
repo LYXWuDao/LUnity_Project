54b9e74739f24ac5997de2a1f72dd66494a9cff4
LYXWuDao <2858535420@qq.com>
2015-12-26 17:01:53 +0800

    修改游戏启动配置

--- Assets/Scripts/GameApp.cs
+++ Assets/Scripts/GameApp.cs
@@ -14,14 +14,37 @@ public class GameApp : ALBehaviour
 {
 
     /// <summary>
+    /// 3d  主摄像机
+    /// </summary>
+    private static Camera _mainCamera = null;
+
+    public static Camera MainCamera
+    {
+        get
+        {
+            return _mainCamera;
+        }
+    }
+
+    /// <summary>
     /// 初始化游戏
     /// </summary>
     public override void Awake()
     {
         DontDestroyOnLoad(gameObject);
 
+        // 创建根节点， 所有动态创建的东西都放到这个下面， 包括 ui界面
+        GameObject gameRoot = SLCompHelper.Create("3d Root");
+        DontDestroyOnLoad(gameRoot);
+
+        Camera cam = FindObjectOfType<Camera>();
+        cam.transform.parent = gameRoot.transform;
+        _mainCamera = cam;
+
         // 创建2d ui 相机
         UIPanel panel = NGUITools.CreateUI(false);
+        DontDestroyOnLoad(panel.gameObject);
+
         UIRoot uiRoot = panel.GetComponent<UIRoot>();
         uiRoot.scalingStyle = UIRoot.Scaling.Constrained;
         // 找到创建的 ui 相机
@@ -33,7 +56,6 @@ public class GameApp : ALBehaviour
         if (root == null) return;
         SLUIManage.UIRoot = root.transform;
         // 加载各种资源
-
 
 
         // 资源加载完毕打开登陆界面
@@ -48,6 +70,21 @@ public class GameApp : ALBehaviour
 
     // Update is called once per frame
     public override void OnUpdate(float deltaTime)
+    {
+
+    }
+
+    public override void OnClear()
+    {
+        _mainCamera = null;
+    }
+
+    public override void OnApplicationPause()
+    {
+
+    }
+
+    public override void OnApplicationQuit()
     {
 
     }
